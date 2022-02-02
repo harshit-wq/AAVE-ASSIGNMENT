5d8a7d36e4748ca11a9b69205b23121cfac8288f
harshit-wq <55633714+harshit-wq@users.noreply.github.com>
2022-02-02 23:01:22 +0530

    Add files via upload

--- interface.sol
+++ interface.sol
@@ -12,3 +12,23 @@ interface aave_functions{
   function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external returns (uint256);
 
 }
+
+
+interface IERC20_functions {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+}
--- main.sol
+++ main.sol
@@ -1,4 +1,8 @@
 //SPDX-License-Identifier: GPL-3.0
+
+// the comment out are the mistakes i was doing , i commented them instead of removing them so that you know how i proceeded over them
+
+
 pragma solidity ^0.8.3;
 
 import "./interface.sol";
@@ -6,35 +10,54 @@ import "./interface.sol";
 contract interact{
 
     address private interact_with = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
-    uint8 private check=0;
+    //uint8 private check=0; - no use as i have require statement on transferFrom
+
+    // i had to comment out onBehalfOf because it was not being used do let me know if it is wrong
 
 
-    function isupply(address _asset, uint256 _amount, address _onBehalfOf, uint16 _referralCode) external {
-        require(check==1);
+    function isupply(address _asset, uint256 _amount/*, address _onBehalfOf*/, uint16 _referralCode) external {
+        //require(check==1);
+
+        IERC20_functions token = IERC20_functions(_asset);  //is this address asset used right?
+
+        require(token.balanceOf(msg.sender)>=_amount , "You do not have enough balance.");         //i have used the require statmenet because they are all bool
+        //require(token.approve(_onBehalfOf,_amount)); // realised it's wrong to include this, the user needs to approve to me
+        require(token.transferFrom(msg.sender, address(this), _amount));
+
         aave_functions instance = aave_functions(interact_with);
-        instance.deposit(_asset, _amount, _onBehalfOf, _referralCode);
-        check=0;
+        instance.deposit(_asset, _amount, address(this), _referralCode); //should this onBehalfOf be the address of this contract
+        //check=0;
     }
 
     function iwithdraw(address asset, uint256 amount, address to) external {
-        require(check==1);
+        //require(check==1);
+        IERC20_functions token = IERC20_functions(asset);
         aave_functions instance = aave_functions(interact_with);
-        instance.withdraw(asset, amount, to);
-        check=0;
+        instance.withdraw(asset, amount, address(this)); //this contract will withdraw money
+        token.transfer(to,amount);
+        //check=0;
     }
 
-    function iborrow(address asset, uint256 amount,uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external {
-        require(check==1);
+    function iborrow(address asset, uint256 amount,uint256 interestRateMode, uint16 referralCode/*, address onBehalfOf*/) external {
+        //require(check==1);
+        IERC20_functions token = IERC20_functions(asset);
         aave_functions instance = aave_functions(interact_with);
-        instance.borrow(asset, amount, interestRateMode, referralCode, onBehalfOf);
-        check=0;
+        instance.borrow(asset, amount, interestRateMode, referralCode, address(this));
+        token.transfer(msg.sender,amount);
+        //check=0;
     }
 
-    function irepay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external {
-        require(check==1);
+    function irepay(address asset, uint256 amount, uint256 rateMode/*, address onBehalfOf*/) external {
+        //require(check==1);
+        IERC20_functions token = IERC20_functions(asset);  //is this address asset used right?
+
+        require(token.balanceOf(msg.sender)>=amount , "You do not have enough balance.");         //i have used the require statmenet because they are all bool
+        //require(token.approve(onBehalfOf,amount)); // realised it's wrong to include this, the user needs approve to me
+        require(token.transferFrom(msg.sender, address(this), amount));
+
         aave_functions instance = aave_functions(interact_with);
-        instance.repay(asset, amount, rateMode, onBehalfOf);
-        check=0;
+        instance.repay(asset, amount, rateMode, address(this));
+        //check=0;
     }
 
 }
